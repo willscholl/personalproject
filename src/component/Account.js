6133424e00549399a6e02865249a05015d82e974
William Scholl <willscholl@me.com>
2019-03-10 21:28:59 -0600

    account layout started

--- src/component/Account.js
+++ src/component/Account.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import "./Account.css"
 class Account extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      profilepic: ''
+    }
+  }
   render() {
     return(
       <div className='account-wrapper'>
-        <div>
-          
+        <div className='account-paper'>
+          <div className='account-profilepic'>
+          </div>
+          <div className='account-username'>
+            <p>Username</p>
+          </div>
+          <div className='account-password'>
+            Password or Button
+          </div>
+          <div className='account-emailnotfication'>
+            Email Notifications Switch
+          </div>
+          <div className='save-button-wrapper'>
+            <button className='save-button'>Save</button>
+          </div>
         </div>
       </div>
     )
